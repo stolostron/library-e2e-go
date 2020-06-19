fe710ef70d95ddd7427a823255e65c0cc7e2f439
Dominique Vernier <dvernier@redhat.com>
2020-06-19 16:07:45 -0400

    Change options format

--- pkg/options/hub.go
+++ pkg/options/hub.go
@@ -6,12 +6,12 @@ import (
 )
 
 //GetHubKubeConfig returns the hub kubeconfig path if exists and empty string if not
-//The kubeconfig file for the hub is supposed to be in <configDir>/<scenario>/<hubName>/kubeconfig
-func GetHubKubeConfig(configDir, scenario string) string {
-	if configDir == "" || scenario == "" {
+//The kubeconfig file for the hub is supposed to be in <configDir>/kubeconfig
+func GetHubKubeConfig(configDir string) string {
+	if configDir == "" {
 		return ""
 	}
-	kubeConfigFilePath := filepath.Join(configDir, scenario, "kubeconfig.yaml")
+	kubeConfigFilePath := filepath.Join(configDir, "kubeconfig.yaml")
 	if _, err := os.Stat(kubeConfigFilePath); os.IsNotExist(err) {
 		kubeConfigFilePath = ""
 	}
